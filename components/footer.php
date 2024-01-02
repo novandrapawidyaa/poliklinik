f7b3955fa02fc357a066fed3d9748edb942bd41c
novandrapawidyaa <novandrapawidya@gmail.com>
2024-01-02 19:31:38 +0700

    Poliklinik

---
+++ components/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      For the Better Future
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2023 <a href="https://adminlte.io">Ilyas Kurnia Ramadhan</a>.</strong> All rights reserved.
+  </footer>
